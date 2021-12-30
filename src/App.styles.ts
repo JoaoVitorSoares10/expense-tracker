f4fc2d1c0decaa00fc83bd2c3c7f5789d12cdd52
João Vitor de Morais Soares <joao.soares@BRASTERAPICA.BR>
2021-12-30 16:09:01 -0300

    Created header component

---
+++ src/App.styles.ts
@@ -0,0 +1,24 @@
+import Styled from 'styled-components';
+
+export const Container = Styled.div`
+    
+`;
+
+export const Header = Styled.div`
+    background-color: darkblue;
+    height: 150px;
+    text-align: center;
+`;
+
+export const HeaderText = Styled.h1`
+    margin: 0;
+    padding: 0;
+    color: #fff;
+    padding-top: 30px;
+`;
+
+export const Body = Styled.div`
+    margin: auto;
+    max-width: 980px;
+    margin-bottom: 30px;
+`;
